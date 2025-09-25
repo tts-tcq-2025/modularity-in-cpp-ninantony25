cfa1786dd29efa8c2f86add2ea3de975567eefb5
ninantony25 <57633711+ninantony25@users.noreply.github.com>
2025-09-26 00:43:54 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
+#include "Test.h"
 #include "ReferenceManual.h"
-#include "test.cpp"
 
-int main() {
-    testNumberToPair(4, TelCoColorCoder::MajorColor::WHITE, TelCoColorCoder::MinorColor::BROWN);
-    testNumberToPair(5, TelCoColorCoder::MajorColor::WHITE, TelCoColorCoder::MinorColor::SLATE);
+int main() 
+{
+    TelCoColorCoder::testNumberToPair(4, TelCoColorCoder::MajorColor::WHITE, TelCoColorCoder::MinorColor::BROWN);
+    TelCoColorCoder::testNumberToPair(5, TelCoColorCoder::MajorColor::WHITE, TelCoColorCoder::MinorColor::SLATE);
 
-    testPairToNumber(TelCoColorCoder::MajorColor::BLACK, TelCoColorCoder::MinorColor::ORANGE, 12);
-    testPairToNumber(TelCoColorCoder::MajorColor::VIOLET, TelCoColorCoder::MinorColor::SLATE, 25);
+    TelCoColorCoder::testPairToNumber(TelCoColorCoder::MajorColor::BLACK, TelCoColorCoder::MinorColor::ORANGE, 12);
+    TelCoColorCoder::testPairToNumber(TelCoColorCoder::MajorColor::VIOLET, TelCoColorCoder::MinorColor::SLATE, 25);
 
     TelCoColorCoder::printColorCodeManual();
     return 0;
